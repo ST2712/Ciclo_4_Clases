26dc4f2c89e7ce4eb0d870f4709c4aba0a3f0cb5
ST2712 <santiagotrujillos2712@gmail.com>
2022-10-13 23:57:59 -0500

    Fin clase_3

--- back/app.js
+++ back/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+//Importar rutas
+const productos = require("./routes/products")
+
+app.use('/api', productos) //Ver si se va a cambiar
+
 module.exports = app
--- back/server.js
+++ back/server.js
@@ -1,1 +1,9 @@
-const app = require("app")
+const app = require("./app")
+
+//Setear el archivo de configuracion
+const dotenv = require("dotenv")
+dotenv.config({path: 'back/config/config.env'})
+
+const sever = app.listen(process.env.PORT, () =>{
+    console.log(`Servidor iniciando en el puerto: ${process.env.PORT}, en modo ${process.env.NODE_ENV}`)
+})
