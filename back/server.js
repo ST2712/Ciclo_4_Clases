b5a06da017df4f1366b9907144bd0fa6f6381b88
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-10-15 13:29:06 -0500

    Conexion a db, fin clase 4

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app = require("./app")
+const connectDatabase = require("./config/database")
 
 //Setear el archivo de configuracion
 const dotenv = require("dotenv")
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurando la base de datos
+connectDatabase();
 
 const sever = app.listen(process.env.PORT, () =>{
     console.log(`Servidor iniciando en el puerto: ${process.env.PORT}, en modo ${process.env.NODE_ENV}`)
