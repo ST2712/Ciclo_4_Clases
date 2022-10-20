506b4bb57623b5b7a733c5ac55538a6aaac394a2
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-10-19 23:44:25 -0500

    Metodos getById, put y delete. Fin clase 6

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,11 +1,76 @@
 const producto = require("../models/prodctos")
+const fetch = (url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); //Usurpacion del require
 
 //Ver la lista de productos
-exports.getProducts = (req,res,next) =>{
+exports.getProducts = async (req,res,next) =>{
+
+    const productos = await producto.find();
     res.status(200).json({
         success:true,
-        message: "En esta ruta usted va a poder ver todos los productos"
+        count : productos.length,
+        productos
     })
+}
+
+//Ver un producto por id
+exports.getProductById = async (req, res, next) =>{
+
+    const product = await producto.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        message: "Aqui debajo encuentras informacion sobre tu producto: ",
+        product
+    })
+}
+
+//Update un producto
+exports.updateProduct = async (req, res, next) =>{
+
+    let product = await producto.findById(req.params.id) //variable modificable
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+
+    product = await producto.findByIdAndUpdate(req.params.id, req.body,{
+
+        new:true, //Asume los valores nuevos que va a registrar
+        runValidators:true //Validar lo que es nuevo
+    });
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
+
+//Eliminar un producto
+exports.deleteProduct = async (req, res, next) =>{
+
+    const product = await producto.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+
+        success:true,
+        message:"Producto eliminado correctamente"
+    })
+
 }
 
 //Crear nuevo producto /api/productos
@@ -17,3 +82,24 @@ exports.newProduct=async(req,res,next)=>{
         product
     })
 }
+
+//PROBANDO FETCH
+//Ver todos los productos
+function verProductos(){
+
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+//verProductos(); Se llama al metodo y se devuelve por consola
+
+//Ver por id
+function verProductoPorId(id){
+
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+//verProductoPorId("634b2877688be57fa7ccd72c"); Se llama al metodo
