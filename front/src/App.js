a6314f993ea3b114f67bca417e7afd4d10c408fb
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-10-21 11:53:56 -0500

    Clase 7 hasta el logo funcionando, pre borrado de archivos

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de mascotas</h1>
+      <img src="./images/logo-tienda-mascotas.png" alt="Nada"></img>
+    </div>
+  );
+}
+
+export default App;
