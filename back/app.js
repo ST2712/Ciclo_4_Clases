071d9219ce2a72a732290470c101f3e719b4170f
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-11-03 12:01:54 -0500

    Fin_clase 14, se agrega el modelo de usuarios

--- back/app.js
+++ back/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require('./middleware/errors')
 
 app.use(express.json());
 
 //Importar rutas
 const productos = require("./routes/products")
+const usuarios = require("./routes/auth")
 
 app.use('/api', productos) //Ver si se va a cambiar
+app.use('/api',usuarios)
+
+//MiddleWare para manejo de errores
+app.use(errorMiddleware)
 
 module.exports = app
--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,54 +1,45 @@
-const producto = require("../models/prodctos")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const producto = require("../models/prodctos");
+const ErrorHandler = require("../utils/errorHandler");
 const fetch = (url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); //Usurpacion del require
 
 //Ver la lista de productos
-exports.getProducts = async (req,res,next) =>{
+exports.getProducts = catchAsyncErrors(async (req,res,next) =>{
 
     const productos = await producto.find();
 
-    if(!productos){
-        return res.status(404).json({
-            success:false,
-            error:true
-        })
-    }
+    if (!productos){
+        return next(new ErrorHandler("Productos no encontrado", 404))
+        }
 
     res.status(200).json({
         success:true,
         cantidad : productos.length,
         productos
     })
-}
+})
 
 //Ver un producto por id
-exports.getProductById = async (req, res, next) =>{
+exports.getProductById= catchAsyncErrors( async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
 
-    const product = await producto.findById(req.params.id)
-    if(!product){
-        return res.status(404).json({
-            success: false,
-            message: "No encontramos ese producto",
-            error:true
-        })
-    }
-
+    if (!product){
+        return next(new ErrorHandler("Producto no encontrado", 404))
+        }
     res.status(200).json({
         success:true,
-        message: "Aqui debajo encuentras informacion sobre tu producto: ",
+        message:"Aqui debajo encuentras información sobre tu producto: ",
         product
     })
-}
+})
 
 //Update un producto
-exports.updateProduct = async (req, res, next) =>{
+exports.updateProduct = catchAsyncErrors(async (req, res, next) =>{
 
     let product = await producto.findById(req.params.id) //variable modificable
-    if(!product){
-        return res.status(404).json({
-            success: false,
-            message: "No encontramos ese producto"
-        })
-    }
+    if (!product){
+        return next(new ErrorHandler("No se encontró el producto", 404))
+        }
 
     product = await producto.findByIdAndUpdate(req.params.id, req.body,{
 
@@ -60,18 +51,15 @@ exports.updateProduct = async (req, res, next) =>{
         message:"Producto actualizado correctamente",
         product
     })
-}
+})
 
 //Eliminar un producto
-exports.deleteProduct = async (req, res, next) =>{
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) =>{
 
     const product = await producto.findById(req.params.id)
-    if(!product){
-        return res.status(404).json({
-            success: false,
-            message: "No encontramos ese producto"
-        })
-    }
+    if (!product){
+        return next(new ErrorHandler("Producto no encontrado", 404))
+        }
 
     await product.remove();
     res.status(200).json({
@@ -80,17 +68,17 @@ exports.deleteProduct = async (req, res, next) =>{
         message:"Producto eliminado correctamente"
     })
 
-}
+})
 
 //Crear nuevo producto /api/productos
-exports.newProduct=async(req,res,next)=>{
+exports.newProduct= catchAsyncErrors(async(req,res,next)=>{
     const product=await producto.create(req.body);
 
     res.status(201).json({
         success:true,
         product
     })
-}
+})
 
 //PROBANDO FETCH
 //Ver todos los productos
